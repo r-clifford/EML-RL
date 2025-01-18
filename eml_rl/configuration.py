8fa184f386eab828f61284bda8e9a19155ff985f
r-clifford <r-clifford@example.com>
2025-01-18 00:09:12 -0500

    Document configuration.py

--- eml_rl/configuration.py
+++ eml_rl/configuration.py
@@ -1,9 +1,31 @@
 from eml_rl.reward_functions.ProgressReward import ProgressReward
 from eml_rl.reward_functions.ScaledReward import ScaledReward
 
+
+# Max and min velocity in m/s
+# Ensure vmin > 0.0 to avoid stopping/reversing
+# Higher vmax may allow better laptimes, though training is often
+# significantly slower as model crashes a lot more
 vmax = 4.0
 vmin = 1.0
+
+# Number of lidar observations to stack
+# May allow for model to learn temporal understanding
+# as the lidar scans a t = 0 -> t = `frame_stack` are concatenated
+# Keep in mind that a scan is retrieved every ~30 ms
+# Recommended 5-40
 frame_stack = 5
+
+# Number of lidar beams to sample
+# Unmodified scan is 1080 beams
+# We linearly sample `lidar_beams` from the full scan
+# Recommended on of [20, 40, 80]
 lidar_beams = 80
 
+# Reward function to use
+# Available reward functions in `git_root`/eml_rl/reward_functions
+# Make sure to import requested function
+# Example:
+# from eml_rl.reward_functions.ProgressReward import ProgressReward
 reward_function = ProgressReward
+
